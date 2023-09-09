e27cb2fd95b099af99e561cfdb321a66a6fb3d1a
Pavan-Sai-padavala <72682254+Pavan-Sai-padavala@users.noreply.github.com>
2023-09-09 17:29:46 +0530

    Update Projects.tsx

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -10,11 +10,12 @@ export const Projects = () => {
       <div className="project_container">
         <div className="projectTitle">Portfolio Resume</div>
         <ul className="projectDetails">
-          <li>Built from scratch using HTML,CSS,JavaScript and Jquery</li>
-          <li>added dynamic features such as mouse-click events,page loading and animation</li>
+          <li>Created a Responsive, Dynamic Single-page application using react js framework</li>
+          <li>Added bottom navigation bar for smoother user experience</li>
+          <li>Developed a nice UI with Material UI</li>
         </ul>
         <div className="projectLinks">
-          <Link href="https://github.com/Pavan-Sai-padavala/Pavan-sai-padavala.github.io" target="_blank" rel="noopener noreferrer"><SocialIcon style={{width:'2rem',height:'2rem'}} network='github' /></Link>
+          <Link href="https://github.com/Pavan-Sai-padavala/PORTFOLIO" target="_blank" rel="noopener noreferrer"><SocialIcon style={{width:'2rem',height:'2rem'}} network='github' /></Link>
           <Link href="https://www.youtube.com/@pavansaipadavala46" target="_blank" rel="noopener noreferrer"><SocialIcon style={{width:'2rem',height:'2rem'}} network='youtube' /></Link>  
         </div>
       </div>
