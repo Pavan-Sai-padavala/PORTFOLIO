93b59f53dc4e82f587ed1a5d76f4eccce42581ee
Pavan-Sai-padavala <pavansaipadavala@gmail.com>
2023-10-23 15:47:23 +0530

    Project section fix and responsive fix
    Changed project section to version 2.0 edition

--- src/components/ProjectItem.tsx
+++ src/components/ProjectItem.tsx
@@ -1,26 +1,34 @@
-import { Link } from '@mui/material'
-import React from 'react'
+import { Button, Link, Typography } from '@mui/material'
+import React, { useState } from 'react'
 import { NavLink } from 'react-router-dom'
 import { SocialIcon } from 'react-social-icons'
 type projectItems={
     title:String,
-    description:String[],
-    links:String[]
+    description:String[];
+    status:boolean;
+    links?:string[];
+    stack:String[]
 }
 const ProjectItem = (props:projectItems) => {
+  const [expand,setExpand]=useState(false);
   return (
-    <div className="project_container">
-        <div className="projectTitle">{props.title}</div>
+    <div className="project_container" onMouseEnter={()=>setExpand(true)} onMouseLeave={()=>setExpand(false)}>
+        <div className="projectTitle">
+          <Typography variant="h6" style={{padding:"0 20px 0 20px"}}>{props.title}</Typography>
+          {props.status && <div className='projectStatus'>Working</div>}
+        </div>
         <ul className="projectDetails">
           {props.description.map((line)=><li>{line}</li>)}
         </ul>
-        <div className="projectLinks">
-        {/* 
-          {props.links.map((link)=><Link href={link} target="_blank" rel="noopener noreferrer">link</Link>)} */}
-          {/* <Link href="https://github.com/Pavan-Sai-padavala/PORTFOLIO" target="_blank" rel="noopener noreferrer"><SocialIcon style={{width:'2rem',height:'2rem'}} network='github' /></Link>
-          <Link href="https://www.youtube.com/@pavansaipadavala46" target="_blank" rel="noopener noreferrer"><SocialIcon style={{width:'2rem',height:'2rem'}} network='youtube' /></Link>   */}
+        {expand && props.links && <div className="projectLinks">
+          <Link href={props.links[0]} target="_blank" rel="noopener noreferrer"><SocialIcon style={{width:'2rem',height:'2rem'}} network='github' /></Link>
+          <Link href={props.links[1]} target="_blank" rel="noopener noreferrer"><SocialIcon style={{width:'2rem',height:'2rem'}} network='youtube' /></Link>
+        </div>}
+        <div className='projectTechStack'>
+          <div style={{padding:"5px 0 5px 0"}}>Tech Stack</div>
+          <div>{props.stack.map((stackItem)=><Button>{stackItem}</Button>)}</div>
         </div>
-      </div>
+    </div>
   )
 }
 
--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -2,23 +2,29 @@ import React from 'react'
 import BottomNav from '../components/BottomNav'
 import { Link, Typography } from '@mui/material'
 import { SocialIcon } from 'react-social-icons'
+import ProjectItem from '../components/ProjectItem'
 
 export const Projects = () => {
   return (
     <><div className="cardContainer">
-      <Typography variant='h4' component='h4'>Projects</Typography>
-      <div className="project_container">
-        <div className="projectTitle">Portfolio Resume</div>
-        <ul className="projectDetails">
-          <li>Created a Responsive, Dynamic Single-page application using react js framework</li>
-          <li>Added bottom navigation bar for smoother user experience</li>
-          <li>Developed a nice UI with Material UI</li>
-        </ul>
-        <div className="projectLinks">
-          <Link href="https://github.com/Pavan-Sai-padavala/PORTFOLIO" target="_blank" rel="noopener noreferrer"><SocialIcon style={{width:'2rem',height:'2rem'}} network='github' /></Link>
-          <Link href="https://www.youtube.com/@pavansaipadavala46" target="_blank" rel="noopener noreferrer"><SocialIcon style={{width:'2rem',height:'2rem'}} network='youtube' /></Link>  
-        </div>
-      </div>
+      <Typography style={{textAlign:'center'}}variant='h4' component='h4'>Work Samples</Typography>
+      <ProjectItem title="Portfolio" status={false}
+                   description={["Created a Single-page, Dynamic and Responsive application from scratch using React Js","Added bottom navigation bar for smoother navigating experience","Developed a nice interactive UI with Material UI"]}
+                   links={["https://github.com/Pavan-Sai-padavala/PORTFOLIO","https://www.youtube.com/@pavansaipadavala46"]} 
+                   stack={["React","Material UI","SPA"]}/>
+      <ProjectItem title="Digi Pustak" status={true}
+                   description={["Developed a Landing page focussed to highlight the product features","working on Login interfaces & profile page"]} 
+                   stack={["React","Node Js","Express.js"]}/>
+      
+      <Typography style={{margin:"40px 0 5px 0",textAlign:'center'}}variant='h4' component='h4'>Open-Source contributions</Typography>
+      <ProjectItem title="A Web Based Code Editor" status={false}
+                   description={["A code editor that compiles over 40+ Languages","Contributed Coding Focussed UI","Enhanced the footer section and added credits section"]}
+                   links={["https://github.com/Pavan-Sai-padavala/PORTFOLIO","https://www.youtube.com/@pavansaipadavala46"]} 
+                   stack={["Tailwind CSS","React","Monaco Code Editor","Judge0 API"]}/>
+      <ProjectItem title="Web Componentify" status={false}
+                   description={["A open-source repository,providing a collection of re-usable react componenets","Contributed bottom navigation componenet to simplify the navigating experience"]}
+                   links={["https://github.com/Pavan-Sai-padavala/PORTFOLIO","https://www.youtube.com/@pavansaipadavala46"]} 
+                   stack={["React","JSX","CSS3","SPA"]}/>
     </div><BottomNav back={'/Home/About'} next={'/Home/Skills'} nextName={'SKILLS'} backName={'ABOUT'} /></>      
   )
 }
