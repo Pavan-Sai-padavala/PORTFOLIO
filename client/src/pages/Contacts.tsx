ac66e267c68c5b904e5c3e615c3f21a437ebca37
Pavan-Sai-padavala <pavansaipadavala@gmail.com>
2023-12-25 17:04:10 +0530

    Added deployed end-point url

--- client/src/pages/Contacts.tsx
+++ client/src/pages/Contacts.tsx
@@ -26,7 +26,7 @@ const Contact = () => {
         e.preventDefault();
         try{
             setProcessingState("Sending Message");
-            const response = await fetch("http://localhost:3000/mail", {
+            const response = await fetch("https://backend-service-c1fx.onrender.com/mail", {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json",
