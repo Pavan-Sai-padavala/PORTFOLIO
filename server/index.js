d6b7d1f283453d62d51e1c0895fe44f8859a8616
Pavan-Sai-padavala <pavansaipadavala@gmail.com>
2024-04-01 12:24:07 +0530

    Update index.js
    resolved cors issue

--- server/index.js
+++ server/index.js
@@ -2,7 +2,9 @@ const express=require("express")
 const app=express();
 var cors = require('cors');
 
-app.use(cors());
+app.use(cors({
+    origin:'https://pavanpps.biz'
+}));
 
 app.use(express.json());
 
